6186aa3774632da50cd2329de36abfe1fb8743c6
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2025-06-17 16:37:55 -0300

    feat: button types

--- src/App/styles.js
+++ src/App/styles.js
@@ -3,10 +3,23 @@ import { Platform, StatusBar, StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   wrapper: {
     marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    flex: 1,
   },
   scrollView: {
-    backgroundColor: "#222",
-    height: "100%",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    flex: 1,
   },
-  text: { fontSize: 32, backgroundColor: "green" },
+  buttonsContainer: { gap: 16 },
+  button: {
+    backgroundColor: "#222",
+    height: 48,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonLabel: { color: "#fff", fontWeight: "bold" },
+  buttonDisabled: {
+    backgroundColor: "#ccc",
+  },
 });
