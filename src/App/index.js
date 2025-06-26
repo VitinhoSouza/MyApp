25b58ce91e2d9dbb8d85e3178bfc61e16958e12b
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2025-06-26 09:57:48 -0300

    feat: handling data and UX improvements from Inputs

--- src/App/index.js
+++ src/App/index.js
@@ -1,23 +1,50 @@
-import { StatusBar, SafeAreaView, TextInput, View } from "react-native";
+import { StatusBar, SafeAreaView, View } from "react-native";
 
 import { styles } from "./styles";
 import { Button } from "../componentes/Button";
 import { Input } from "../componentes/Input";
+import { useRef, useState } from "react";
 
 function App() {
-  const disabled = true;
+  const passwordInputRef = useRef();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit() {
+    console.log({ email, password });
+  }
+
   return (
     <SafeAreaView style={styles.wrapper}>
       <View style={styles.container}>
         <StatusBar barStyle="dark-content" />
 
-        <Input placeholder="E-mail" autoFocus />
+        <Input
+          placeholder="E-mail"
+          autoFocus
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+          autoComplete="email"
+          onSubmitEditing={() => passwordInputRef.current.focus()}
+          returnKeyType="next"
+          value={email}
+          onChangeText={setEmail}
+        />
 
-        <Input placeholder="Senha" secureTextEntry />
+        <Input
+          ref={passwordInputRef}
+          placeholder="Senha"
+          keyboardType="numeric"
+          secureTextEntry
+          returnKeyType="done"
+          onSubmitEditing={handleSubmit}
+          value={password}
+          onChangeText={setPassword}
+        />
 
-        <Button disabled={!disabled} onPress={() => alert("Button")}>
-          Oi botão!
-        </Button>
+        <Button onPress={handleSubmit}>Enviar</Button>
       </View>
     </SafeAreaView>
   );
