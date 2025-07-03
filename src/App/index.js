74ecddadc8f018c93b83855d5b8530d61a94c858
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2025-07-03 17:10:30 -0300

    feat: Flatlist (++ perf) and some props

--- src/App/index.js
+++ src/App/index.js
@@ -1,29 +1,37 @@
-import { StatusBar, SafeAreaView, View, Text, ScrollView } from "react-native";
+import { StatusBar, SafeAreaView, View, Text, FlatList } from "react-native";
 
 import { styles } from "./styles";
 
 function App() {
-  const posts = Array.from({ length: 11 }, (value, index) => ({
+  const posts = Array.from({ length: 100 }, (value, index) => ({
     id: Math.random(),
     title: `Post ${index + 1}`,
   }));
 
   return (
     <SafeAreaView style={styles.wrapper}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={{
-          gap: 16,
-          alignItems: "stretch",
-        }}
-      >
+      <View style={styles.container}>
         <StatusBar barStyle="dark-content" />
-        {posts.map((post) => (
-          <View key={post.id} style={styles.postContainer}>
-            <Text style={styles.postTitle}>{post.title}</Text>
-          </View>
-        ))}
-      </ScrollView>
+        <FlatList
+          data={posts}
+          keyExtractor={(post) => post.id}
+          renderItem={({ item: post }) => (
+            <View key={post.id} style={styles.postContainer}>
+              <Text style={styles.postTitle}>{post.title}</Text>
+            </View>
+          )}
+          // contentContainerStyle={{ gap: 16 }}
+          ItemSeparatorComponent={<Text>Separator</Text>}
+          getItemLayout={(data, index) => ({
+            index,
+            length: 64 + 16,
+            offset: index * (64 + 16),
+          })} // ++ FlatList perf
+          ListHeaderComponent={<Text>Header</Text>}
+          ListFooterComponent={<Text>Footer</Text>}
+          ListEmptyComponent={<Text>Empty</Text>}
+        />
+      </View>
     </SafeAreaView>
   );
 }
