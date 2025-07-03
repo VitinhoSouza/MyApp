9fd2c5c9c539d58e4fb0a936cc6a70415a5f35d5
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2025-07-03 16:11:37 -0300

    feat: Lists with ScrollView + map (less perf)

--- src/App/index.js
+++ src/App/index.js
@@ -1,69 +1,29 @@
-import { StatusBar, SafeAreaView, View, Switch } from "react-native";
+import { StatusBar, SafeAreaView, View, Text, ScrollView } from "react-native";
 
 import { styles } from "./styles";
-import { Button } from "../componentes/Button";
-import { Input } from "../componentes/Input";
-import { useRef, useState } from "react";
-import { Textarea } from "../componentes/Textarea";
 
 function App() {
-  const passwordInputRef = useRef();
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [selected, setSelected] = useState(false);
-
-  function handleSubmit() {
-    console.log({ email, password });
-  }
+  const posts = Array.from({ length: 11 }, (value, index) => ({
+    id: Math.random(),
+    title: `Post ${index + 1}`,
+  }));
 
   return (
     <SafeAreaView style={styles.wrapper}>
-      <View style={styles.container}>
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={{
+          gap: 16,
+          alignItems: "stretch",
+        }}
+      >
         <StatusBar barStyle="dark-content" />
-
-        <Switch
-          style={{
-            marginRight: "auto",
-          }}
-          value={selected}
-          onValueChange={setSelected}
-          thumbColor="purple"
-          ios_backgroundColor="yellow"
-          trackColor={{
-            false: "yellow", // * Android only
-            true: "#f00",
-          }}
-        />
-
-        <Textarea placeholder="Descrição" />
-
-        <Input
-          placeholder="E-mail"
-          autoFocus
-          keyboardType="email-address"
-          autoCapitalize="none"
-          autoCorrect={false}
-          autoComplete="email"
-          onSubmitEditing={() => passwordInputRef.current.focus()}
-          returnKeyType="next"
-          value={email}
-          onChangeText={setEmail}
-        />
-
-        <Input
-          ref={passwordInputRef}
-          placeholder="Senha"
-          keyboardType="numeric"
-          secureTextEntry
-          returnKeyType="done"
-          onSubmitEditing={handleSubmit}
-          value={password}
-          onChangeText={setPassword}
-        />
-
-        <Button onPress={handleSubmit}>Enviar</Button>
-      </View>
+        {posts.map((post) => (
+          <View key={post.id} style={styles.postContainer}>
+            <Text style={styles.postTitle}>{post.title}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </SafeAreaView>
   );
 }
