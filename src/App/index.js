d40451b8b9852a0b34d9611c0b2c69b81fe9680e
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2025-10-23 08:57:10 -0300

    feat: AlertAPI examples

--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,11 @@
-import { StatusBar, SafeAreaView, View, Modal, Text } from "react-native";
+import {
+  StatusBar,
+  SafeAreaView,
+  View,
+  Modal,
+  Text,
+  Alert,
+} from "react-native";
 
 import { Button } from "../componentes/Button";
 
@@ -7,12 +14,69 @@ import { useState } from "react";
 
 function App() {
   const [visible, setVisible] = useState(false);
+
+  function handleShowAlert() {
+    Alert.alert(
+      "Atenção!",
+      "Bloquear o usuário fará com que ele perca acesso ao sistema",
+      [
+        {
+          text: "Cancelar",
+          isPreferred: true, //iOS only
+          onPress: () => console.log("Bloquear usuário"),
+          style: "cancel",
+        },
+        {
+          text: "Bloquear",
+          onPress: () => console.log("Bloquear usuário"),
+          style: "destructive", //iOS only
+        },
+        {
+          text: "Depois",
+          onPress: () => console.log("Bloquear usuário"),
+        },
+      ],
+      {
+        cancelable: true, // Android only
+        onDismiss: () => console.log("Alert dimissed"), // Android only
+        userInterfaceStyle: "light", // iOS only
+      }
+    );
+  }
+
+  function handleShowPrompt() {
+    // iOS only
+    Alert.prompt(
+      "Responda:",
+      "Qual o melhor curso de interwebs?",
+      [
+        {
+          text: "Cancelar",
+          style: "cancel",
+        },
+        {
+          text: "Enviar",
+          onPress: (value) => console.log("Valor enviado:", value),
+          isPreferred: true,
+        },
+      ],
+      "login-password",
+      "Valor default",
+      "email-address",
+      {
+        userInterfaceStyle: "dark",
+      }
+    );
+  }
+
   return (
     <>
       <SafeAreaView style={styles.wrapper}>
         <StatusBar barStyle="dark-content" animated />
         <View style={styles.container}>
           <Button onPress={() => setVisible(true)}>Abrir modal</Button>
+          <Button onPress={handleShowAlert}>Mostrar alerta</Button>
+          {} <Button onPress={handleShowPrompt}>Mostrar prompt</Button>
         </View>
       </SafeAreaView>
       <Modal
