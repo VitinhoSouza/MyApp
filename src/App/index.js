7713fd5b86daa5fc64b97f735724f9bc01096ead
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2025-11-20 10:59:05 -0300

    feat: InputAccessoryView component (iosOnly)

--- src/App/index.js
+++ src/App/index.js
@@ -1,47 +1,25 @@
 import {
-  DrawerLayoutAndroid,
+  InputAccessoryView,
   SafeAreaView,
   StatusBar,
-  Text,
   View,
 } from "react-native";
 
 import { Button } from "../componentes/Button";
-
-import { useRef } from "react";
+import { Input } from "../componentes/Input";
 import { styles } from "./styles";
 
 function App() {
-  const drawerRef = useRef(null);
-
-  function handleOpenMenu() {
-    drawerRef.current?.openDrawer();
-  }
-
-  function handleCloseMenu() {
-    drawerRef.current?.closeDrawer();
-  }
-
   return (
     <SafeAreaView style={styles.wrapper}>
-      <DrawerLayoutAndroid
-        ref={drawerRef}
-        drawerPosition="right"
-        drawerBackgroundColor="rgba(255, 0, 0, 0.5)"
-        drawerWidth={200}
-        drawerLockMode="locked-open"
-        renderNavigationView={() => (
-          <View>
-            <Text>Oi meu chapa!</Text>
-            <Button onPress={handleCloseMenu}>Fechar menu</Button>
-          </View>
-        )}
-      >
-        <StatusBar barStyle="dark-content" animated />
-        <View style={styles.container}>
-          <Button onPress={handleOpenMenu}>Abrir menu</Button>
-        </View>
-      </DrawerLayoutAndroid>
+      <StatusBar barStyle="dark-content" animated />
+      <View style={styles.container}>
+        <Input placeholder="Nome" inputAccessoryViewID="name" />
+
+        <InputAccessoryView nativeID="name">
+          <Button>Accessory do Nome</Button>
+        </InputAccessoryView>
+      </View>
     </SafeAreaView>
   );
 }
